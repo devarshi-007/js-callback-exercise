17d1ac6eef599b09ef63e7018e0afab475af903f
devarshi007 <devarshi.trivedi@improwised.com>
2023-04-11 20:04:18 +0530

    Callback task completed

--- callback.js
+++ callback.js
@@ -1,12 +1,17 @@
-function sum(n1,n2,cb){
+const InputArray= [
+    1,2,3,4,5,6,7,8
+]
+const mul = []
+
+function sum(n1,n2,j,cb){
     setTimeout(function(){
-        cb(n1+n2);
+        cb(n1+n2,j);
     },100)
 }
 
-function multiply(n1,n2,cb){
+function multiply(n1,n2,i,cb){
     setTimeout(function(){
-        cb(n1*n2);
+        cb(n1*n2,i);
     },200)
 }
 
@@ -14,8 +19,24 @@ function print(str){
     console.log(str);
 }
 
-multiply(5,4,function(firstResult){
-    multiply(3,2,function(secondResult){
-        sum(firstResult,secondResult,print);
-    })
-})
+let i=0;
+let j=0;
+
+
+multiply(InputArray[i],InputArray[i+1],i+2,use)
+function use(result,i){
+    mul.push(result)
+    if (i >= InputArray.length){
+        useSum(mul[j],j)
+    } else {
+        multiply(InputArray[i],InputArray[i+1],i+2,use)
+    }
+}
+
+function useSum(result,j){
+    if (j==(mul.length-1)){
+        sum(result,mul[j],j+1,print)
+    } else {
+        sum(result,mul[j+1],j+1,useSum)
+    }
+}
